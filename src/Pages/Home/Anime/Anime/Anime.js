8ef6f2e7002877c37070081fc835014305107655
shahariyer-as <shahariyer.as@gmail.com>
2022-02-20 11:47:50 +0600

    pie-chart-add

--- src/Pages/Home/Anime/Anime/Anime.js
+++ src/Pages/Home/Anime/Anime/Anime.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Anime.css';
 
-const Anime = () => {
+const Anime = (props) => {
+    const { title, mal_id, episodes, image_url } = props.sr;
+    console.log(props.sr)
     return (
         <div>
-            <div>sdf</div>
+            <div className="row ">
+                <div className="col-md-4 gap-3 py-3 col-sm-6">
+                    <div className="box container">
+                        <Link to=''>
+                            <img className='anime-img' src={image_url} width={130} height={100} alt='' />
+                        </Link>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 };
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,58 +1,57 @@
 import { click } from '@testing-library/user-event/dist/click';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 import Footer from '../Shared/Footer/Footer';
 import Nav from '../Shared/Nav/Nav';
+import Anime from './Anime/Anime/Anime';
+import PieChart from './Anime/Anime/PieChart';
 import './Home.css';
 
 const Home = () => {
-    const [search, setSearch] = useState('')
-    console.log(search)
+    const [search, setSearch] = useState([])
+    // console.log(search.results)
     const typeOn = async (e) => {
         click = (e);
-        console.log(click)
+        // console.log(click)
         const result = await axios.get(`https://api.jikan.moe/v3/search/anime?q=${click},reset()`)
         setSearch(result.data);
 
     }
-    // useEffect(() => {
-    //     axios.get(`https://api.jikan.moe/v3/search/anime?q=${search}`)
-    //         .then(res => {
-    //             setSingleData(res.result)
-    //         })
 
-    // }, [search])
-
-    // const typeOn = (e) => {
-    //     console.log(e)
-    // }
     return (
-        <div>
+        <div >
             <Nav></Nav>
-            <div>
-
-                <div>
-                    <button onClick={() => typeOn('kekkai')}> kakei</button>
-                    <button onClick={() => typeOn('kekkai')}> kakei</button>
-                </div>
-
-
-                {/* <div className="container">
-                    <button type="button" class="drop-button">
-                        ☰
+            <div >
+                <div className="dropdown">
+                    <button className="btn btn-color dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Select Animes
                     </button>
-                    <div class="dropdown">
-                        <ul>
-                            <li>Option 1</li>
-                            <li>Option 2</li>
-                            <li>Option 3</li>
-                            <li>Option 4</li>
-                        </ul>
+                    <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <Link className="dropdown-item" onClick={() => typeOn('kekkai')} to=''> Kakei</Link>
+                        <Link className="dropdown-item" onClick={() => typeOn('naruto')} to=''> Naruto</Link>
+                        <Link className="dropdown-item" onClick={() => typeOn('monster')} to=''> Monster</Link>
+                        <Link className="dropdown-item" onClick={() => typeOn('gintama')} to=''> Gintama</Link>
+                        <Link className="dropdown-item" onClick={() => typeOn('kimetsu')} to=''> Kimetsu</Link>
+                        <Link className="dropdown-item" onClick={() => typeOn('Clannad')} to=''> Clannad</Link>
+
                     </div>
-                </div> */}
+                </div>
+                <div className='home-block'>
+                    <div className='container img-block'>
+                        <div className="container row row-cols-1 pt-3  row-cols-md-3">
+                            {
+                                search?.results?.map(sr => <Anime sr={sr} key={sr.mal_id}></Anime>)
+                            }
+                        </div>
+                    </div>
+                    <div className='container'>
+                        <PieChart></PieChart>
+                    </div>
+                </div>
             </div>
-            <div style={{ paddingTop: '420px' }}>
+            <div style={{ paddingTop: '38px' }}>
                 <Footer></Footer>
             </div>
 
